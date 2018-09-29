33ce25731e9a6a0ffa9c69dd41e9e4cdc887c7f0
Polfer-Shannon <shannon.polfer@gmail.com>
2018-09-28 20:32:39 -0600

    Still trying to figure out heroku

---
+++ web/home/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+Personal Home Page
+-->
+<html lang="en-us">
+    <head>   
+        <meta charset="UTF-8"/>
+        <meta name="author" content="Shannon Polfer">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SP Home</title>
+        <link href="css/home_style.css" rel="stylesheet" type="text/css" 
+              media="screen">
+    </head>
+    <body>
+        <header>
+            <div class="header__text-box">
+                <h1 class="heading-primary">
+                    <span class="heading-primary--main">Shannon's</span>
+                    <span class="heading-primary--sub">CSS313-PHP Assignment Page</span>
+                </h1>
+
+                <a href="#" class="btn btn--white btn--animated">Assignment Pages.</a>
+            </div>
+        </header> 
+    </body>
+</html>
+
