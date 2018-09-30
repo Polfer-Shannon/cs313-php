b5688e3dd2a49615f8f976c579e15df4ca4d5b22
Polfer-Shannon <shannon.polfer@gmail.com>
2018-09-29 19:10:16 -0600

    added some javascript to home page

--- web/home/home.php
+++ web/home/home.php
@@ -10,17 +10,21 @@ Personal Home Page
         <title>SP Home</title>
         <link href="css/home_style.css" rel="stylesheet" type="text/css" 
               media="screen">
+        <script type="text/javascript" src="homejs.js"></script>
     </head>
     <body>
         <header>
             <div class="header__text-box">
                 <h1 class="heading-primary">
                     <span class="heading-primary--main">Shannon's</span>
-                    <span class="heading-primary--sub">CSS313-PHP Assignment Page</span>
+                    <span class="heading-primary--sub">CSS313 Web Engineering II Web Site </span>
                 </h1>
 
-                <a href="#" class="btn btn--white btn--animated">&nbsp;&nbsp;&nbsp;&nbsp;Hi There&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                <a href="#" onclick="myFunction()" class="btn btn--white btn--animated">&nbsp;&nbsp;&nbsp;&nbsp;Hi There&nbsp;&nbsp;&nbsp;&nbsp;</a>
                  <a href="views/assignments.php" class="btn btn--white btn--animated">Assignments</a>
+            </div>
+            <div class="home__btn--message-box">
+                <p id="home__btn--answer"></p>
             </div>
         </header> 
         <footer>
--- web/team/wk2java.js
+++ web/team/wk2java.js
@@ -22,5 +22,6 @@ var textbox_id = "txtColor";
                           
                 
             });
+            
 
 
